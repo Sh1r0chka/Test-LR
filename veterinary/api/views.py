5d8b9f4839f596fa5f2d87da8a1188c288d7fb32
Sh1r0chka <159678826+Sh1r0chka@users.noreply.github.com>
2024-02-11 08:38:41 +0300

    Add files via upload

---
+++ veterinary/api/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from .models import Orders, Categories
+from .serializers import OrderSerializer, UserSerializer, CategoriesSerializer
+from rest_framework import generics, permissions
+from .permissions import IsOwnerOrReadOnly
+
+
+# Информация о пользователях
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Заявки
+class OrdersList(generics.ListCreateAPIView):
+    queryset = Orders.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Orders.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+
+# Категории
+class CategoriesList(generics.ListAPIView):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
+   
